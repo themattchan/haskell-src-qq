738a8e868f3a38ba142cbd6559f48380016f2626
Matthew Chan <matt@awakenetworks.com>
2018-03-28 22:20:19 -0700

    ok

--- src/Language/Haskell/QuasiQuote.hs
+++ src/Language/Haskell/QuasiQuote.hs
@@ -1,9 +1,37 @@
-{-# LANGUAGE QuasiQuotes #-}
 module Language.Haskell.QuasiQuote where
+import Data.Monoid
 
+import qualified Language.Haskell.TH as TH
+import Language.Haskell.TH.Syntax (liftData)
 import Language.Haskell.TH.Quote
 
--- A haskell module
-hsModule :: QuasiQuoter
+-- haskell-src-exts
+import qualified Language.Haskell.Exts.Parser as HsExts
 
-hsSC :: QuasiQuoter
+
+hsModule :: QuasiQuoter
+hsModule = QuasiQuoter quoteExp undefined undefined undefined where
+  quoteExp :: String -> TH.Q TH.Exp
+  quoteExp s = case HsExts.parseModule s of
+    HsExts.ParseFailed srcloc err ->
+      fail ("FAIL: " <>err<> " (at "<>show srcloc<>")")
+    HsExts.ParseOk expr
+      -> liftData expr
+
+hsDecl :: QuasiQuoter
+hsDecl = QuasiQuoter quoteExp undefined undefined undefined where
+  quoteExp :: String -> TH.Q TH.Exp
+  quoteExp s = case HsExts.parseDecl s of
+    HsExts.ParseFailed srcloc err ->
+      fail ("FAIL: " <>err<> " (at "<>show srcloc<>")")
+    HsExts.ParseOk expr
+      -> liftData expr
+
+hsExp :: QuasiQuoter
+hsExp = QuasiQuoter quoteExp undefined undefined undefined where
+  quoteExp :: String -> TH.Q TH.Exp
+  quoteExp s = case HsExts.parseExp s of
+    HsExts.ParseFailed srcloc err ->
+      fail ("FAIL: " <>err<> " (at "<>show srcloc<>")")
+    HsExts.ParseOk expr
+      -> liftData expr
